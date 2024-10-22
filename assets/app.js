e681949dad8439677e6035dd6aee4d743c121dc8
tihomir.ivanov <tihomir.ivanov@siriussoftware.bg>
2024-10-22 17:48:37 +0300

    2 pages done

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,7 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import './styles/index.css';
+import './styles/vehicle.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
---
+++ src/Controller/Controller.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\Routing\Attribute\Route;
+
+class Controller extends AbstractController
+{
+    #[Route('/homepage', name: 'homepage', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('homepage.html.twig', [
+            'vehicles' => $this->getVehicles(),
+        ]);
+    }
+
+    #[Route('/vehicle/{vehicleId}', name: 'vehicle', methods: ['GET'])]
+    public function vehicle(int $vehicleId): Response
+    {
+        $vehicle = $this->getVehicle($vehicleId);
+        $userProfileIds = $this->select("UserProfileId", $this->fetchBids($vehicleId));
+
+        return $this->render('vehiclePage.html.twig', [
+            'vehicle' => $vehicle,
+            'bids' => $this->fetchBids($vehicleId),
+            'userProfiles' => $this->fetchUsers($userProfileIds)
+        ]);
+    }
+
+    private function getVehicles(): array
+    {
+        return [
+            ["Id" => 1, "Currency" => '$', "Title" => "Vehicle One", "Desc" => "Very nice vehicle this one", "HighestBid" => 2002, "Image" => "/assets/vehicle.avif"],
+            ["Id" => 2, "Currency" => '$', "Title" => "Vehicle Two", "Desc" => "Very nice vehicle this one", "HighestBid" => 546346, "Image" => "/assets/vehicle.avif"],
+            ["Id" => 3, "Currency" => '$', "Title" => "Vehicle Three", "Desc" => "Very nice vehicle this one", "HighestBid" => 5257, "Image" => "/assets/vehicle.avif"],
+            ["Id" => 4, "Currency" => '$', "Title" => "Vehicle Four", "Desc" => "Very nice vehicle this one", "HighestBid" => 32532, "Image" => "/assets/vehicle.avif"],
+            ["Id" => 5, "Currency" => '$', "Title" => "Vehicle Five", "Desc" => "Very nice vehicle this one", "HighestBid" => 25002, "Image" => "/assets/vehicle.avif"],
+            ["Id" => 6, "Currency" => '$', "Title" => "Vehicle Six", "Desc" => "Very nice vehicle this one", "HighestBid" => 2131, "Image" => "/assets/vehicle.avif"],
+            ["Id" => 7, "Currency" => '$', "Title" => "Vehicle Seven", "Desc" => "Very nice vehicle this one", "HighestBid" => 32553252, "Image" => "/assets/vehicle.avif"]
+        ];
+    }
+
+    private function getVehicle(int $id): ?array
+    {
+        $vehicles = $this->getVehicles();
+        foreach($vehicles as $vehicle)
+        {
+            if($vehicle['Id'] == $id)
+            {
+                return $vehicle;
+            }
+        }
+
+        return null;
+    }
+
+    private function fetchAllBids(): array
+    {
+        return [
+            ["Id" => 1, "Amount" => "1337", "AuctionId" => 1, "CreatedAt" =>date('Y-m-d h:i:sa',  mktime(10, 55, 30, 21, 9, 2002)), "UserProfileId" => 1],
+            ["Id" => 2, "Amount" => "1337", "AuctionId" => 1, "CreatedAt" =>date('Y-m-d h:i:sa',  mktime(10, 55, 30, 21, 9, 2002)), "UserProfileId" => 1],
+            ["Id" => 3, "Amount" => "1337", "AuctionId" => 1, "CreatedAt" =>date('Y-m-d h:i:sa',  mktime(10, 55, 30, 21, 9, 2002)), "UserProfileId" => 1],
+            ["Id" => 4, "Amount" => "1337", "AuctionId" => 1, "CreatedAt" =>date('Y-m-d h:i:sa',  mktime(10, 55, 30, 21, 9, 2002)), "UserProfileId" => 1],
+            ["Id" => 5, "Amount" => "1337", "AuctionId" => 1, "CreatedAt" =>date('Y-m-d h:i:sa',  mktime(10, 55, 30, 21, 9, 2002)), "UserProfileId" => 1],
+            ["Id" => 6, "Amount" => "1337", "AuctionId" => 1, "CreatedAt" =>date('Y-m-d h:i:sa',  mktime(10, 55, 30, 21, 9, 2002)), "UserProfileId" => 1],
+            ["Id" => 7, "Amount" => "1337", "AuctionId" => 1, "CreatedAt" =>date('Y-m-d h:i:sa',  mktime(10, 55, 30, 21, 9, 2002)), "UserProfileId" => 1],
+            ["Id" => 8, "Amount" => "1337", "AuctionId" => 1, "CreatedAt" =>date('Y-m-d h:i:sa',  mktime(10, 55, 30, 21, 9, 2002)), "UserProfileId" => 1],
+            ["Id" => 9, "Amount" => "1337", "AuctionId" => 4, "CreatedAt" =>date('Y-m-d h:i:sa',  mktime(10, 55, 30, 21, 9, 2002)), "UserProfileId" => 1],
+            ["Id" => 10, "Amount" => "1337", "AuctionId" => 4, "CreatedAt" =>date('Y-m-d h:i:sa',  mktime(10, 55, 30, 21, 9, 2002)), "UserProfileId" => 1],
+            ["Id" => 11, "Amount" => "1337", "AuctionId" => 4, "CreatedAt" =>date('Y-m-d h:i:sa',  mktime(10, 55, 30, 21, 9, 2002)), "UserProfileId" => 1],
+            ["Id" => 12, "Amount" => "1337", "AuctionId" => 5, "CreatedAt" =>date('Y-m-d h:i:sa',  mktime(10, 55, 30, 21, 9, 2002)), "UserProfileId" => 1],
+            ["Id" => 13, "Amount" => "1337", "AuctionId" => 5, "CreatedAt" =>date('Y-m-d h:i:sa',  mktime(10, 55, 30, 21, 9, 2002)), "UserProfileId" => 1],
+            ["Id" => 14, "Amount" => "1337", "AuctionId" => 5, "CreatedAt" =>date('Y-m-d h:i:sa',  mktime(10, 55, 30, 21, 9, 2002)), "UserProfileId" => 1],
+            ["Id" => 15, "Amount" => "1337", "AuctionId" => 5, "CreatedAt" =>date('Y-m-d h:i:sa',  mktime(10, 55, 30, 21, 9, 2002)), "UserProfileId" => 1]
+        ];
+    }
+
+    private function fetchBids(int $auctionId): array
+    {
+        $allBids = $this->fetchAllBids();
+        $auctionBids = [];
+
+        foreach ($allBids as $bid) {
+            if ($bid["AuctionId"] == $auctionId) {
+                $auctionBids[] = $bid;
+            }
+        }
+
+        return $auctionBids;
+    }
+
+    private function fetchAllUsers(): array
+    {
+        return [
+            ['Id' => 1, 'Username' => 'DynamicUsername', 'Picture' => "assets/speedometer.avif"]
+        ];
+    }
+
+    private function fetchUsers(array $userProfileIds): array
+    {
+        $allUsers = $this->fetchAllUsers();
+        $users = [];
+
+        foreach ($allUsers as $user) {
+            if (in_array($user["Id"], $userProfileIds)) {
+                $users[] = $user;
+            }
+        }
+
+        return $users;
+    }
+
+    private function fetchUser(int $userProfileId): ?array
+    {
+        $allUsers = $this->fetchAllUsers();
+
+        foreach ($allUsers as $user) {
+            if ($userProfileId == $user['Id']) {
+                return $user;
+            }
+        }
+
+        return null;
+    }
+
+
+    private function select(string $propertyName, array $collection) : array {
+        $selected = [];
+
+        foreach ($collection as $item) {
+            if(!isset($item[$propertyName])) {
+                continue;
+            }
+
+            $selected[] = $item[$propertyName];
+        }
+
+        return $selected;
+    }
+}
