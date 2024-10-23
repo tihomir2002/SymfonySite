f871e9b4a6165d8fd3618dd7b91f3c50bf5607ab
tihomir.ivanov <tihomir.ivanov@siriussoftware.bg>
2024-10-23 17:39:27 +0300

    about + vehicle styling fixes

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -3,14 +3,12 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Attribute\Route;
 
 class Controller extends AbstractController
 {
-    #[Route('/homepage', name: 'homepage', methods: ['GET'])]
+    #[Route('/', name: 'homepage', methods: ['GET'])]
     public function index(): Response
     {
         return $this->render('homepage.html.twig', [
@@ -29,6 +27,12 @@ class Controller extends AbstractController
             'bids' => $this->fetchBids($vehicleId),
             'userProfiles' => $this->fetchUsers($userProfileIds)
         ]);
+    }
+
+    #[Route('/about', name: 'about', methods: ['GET'])]
+    public function about()
+    {
+        return $this->render('about.html.twig');
     }
 
     private function getVehicles(): array
